0f3d8753a19e4ce1c9a68fc4ed41d917b5a67940
hedgeworth9 <marielle.montero@obf.ateneo.edu>
2023-03-31 17:09:43 +0800

    checklist css update

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.template import loader
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from .models import Property
 from django.views.generic import ListView, DetailView, CreateView, DeleteView
@@ -15,16 +15,30 @@ def home(request):
     return HttpResponse(template.render(context, request))
 
 def signup(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("/signup-success/")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render (request=request, template_name="main/signup.html", context={"register_form":form})
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/signup-success/')
+    else:
+        form = NewUserForm()
+    return render(request, 'main/signup.html', {'register_form': form})
+
+# def signup(request):
+# 	if request.method == "POST":
+# 		form = NewUserForm(request.POST)
+# 		if form.is_valid():
+# 			user = form.save()
+# 			login(request, user)
+# 			messages.success(request, "Registration successful." )
+# 			return redirect("/signup-success/")
+# 		messages.error(request, "Unsuccessful registration. Invalid information.")
+# 	form = NewUserForm()
+# 	return render (request=request, template_name="main/signup.html", context={"register_form":form})
 
 def signup_success(request):
 	template = loader.get_template('main/signup-success.html')
